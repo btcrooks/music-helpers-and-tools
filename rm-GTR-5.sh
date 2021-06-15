d8a5e1e8b28368e91dc6987ce4fbe65851afe002
Brandon Crooks <crookrb@hotmail.com>
2021-06-14 21:36:36 -0400

    Initial Commit

---
+++ rm-GTR-5.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env sh
+
+# Script Setup
+HOME=$(eval echo ~$USER)
+
+# Files
+applicationFiles=(
+  /Library/Preferences/com.native-instruments.Guitar\ Rig\ 6.plist
+  /Applications/Native\ Instruments/Guitar\ Rig\ 6
+)
+pluginFiles=(
+  /Library/Audio/Plug-Ins/Components/Guitar\ Rig\ 6.component
+  /Library/Audio/Plug-Ins/VST/Guitar\ Rig\ 6.vst
+)
+dataSupportFiles=(
+  /Library/Application\ Support/Native\ Instruments/Guitar\ Rig\ 6
+  /Library/Application\ Support/Native\ Instruments/Service\ Center/Guitar\ Rig\ 6.xml
+)
+preferenceFiles=(
+  $HOME/Library/Preferences/com.native-instruments.Guitar\ Rig\ 6.plist
+  $HOME/Library/Application\ Support/Native\ Instruments/Guitar\ Rig\ 6
+)
+
+ALLFILES=(
+  "${applicationFiles[@]}"
+  "${pluginFiles[@]}"
+  "${dataSupportFiles[@]}"
+  "${preferenceFiles[@]}"
+)
+
+for file in "${ALLFILES[@]}"
+do
+  # echo "--> ${file}"
+  # echo
+  # Remove Directories
+  if [ -d "${file}" ]; then
+    echo "Directory: ${file}"
+  fi
+
+  # Remove Files
+  if [ -f "$file" ];then
+    echo "File: ${file}"
+  fi
+
+  # File or diretory not found
+  if [ ! -d "$file" ] && [ ! -f "$file" ]; then
+    echo "File/Dir not found: ${file}"
+  fi
+done
+
+# Appendix
+# https://support.native-instruments.com/hc/en-us/articles/210291865-How-to-Uninstall-Native-Instruments-Software-from-a-Mac-Computer
